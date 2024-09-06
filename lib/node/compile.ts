4c531f7d19fca738e7192b43c8667bbc72a6dcb6
Leah Hirst <leah@hirst.dev>
2024-09-06 20:30:10 +0100

    Update to use word joiner

--- lib/node/compile.ts
+++ lib/node/compile.ts
@@ -4,4 +4,4 @@ export const decompile = (whitespace: string) =>
   whitespace.replaceAll(' ', ZWSP).replaceAll('\t', ZWJ).replaceAll('\n', ZWNJ);
 
 export const compile = (nospace: string) =>
-  nospace.replaceAll(ZWSP, ' ').replaceAll(ZWJ, '\t').replaceAll(ZWNJ, '\n').replaceAll(/\uFEFF.*?\u200D/g, '');
+  nospace.replaceAll(ZWSP, ' ').replaceAll(ZWJ, '\t').replaceAll(ZWNJ, '\n').replace(/\u2060.*?\u200D/g, '');
